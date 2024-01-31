df50e871ee86382c88cde952426a7a6b05c9dfc2
Christopher Karg <christopher_karg@yahoo.co.uk>
2024-01-31 11:25:08 +0000

    refactor using chi to limit type of requests being sent to specific endpoints. Also created visitor count metric

--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
 	// Serve files from the '.' directory at the '/app/' path, after stripping the '/app' prefix from the request path.
 	//eg "/app/assets/logo.png" would serve the file at "./assets/logo.png".
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	// Handle requests to /healthz path
-	mux.HandleFunc("/healthz", handlerReadiness)
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	cfg := &apiConfig{}
+	//configue routes with r.Handle and r.Get
+	r.Handle("/app/*", cfg.middlewareMetricsInc(handler))
+	r.Handle("/app", cfg.middlewareMetricsInc(handler))
+	r.Get("/healthz", handlerReadiness)
+	r.Get("/metrics", cfg.getHitsCountHandler)
+	r.Get("/reset", cfg.resetHitsCountHandler)
 
 	// Serves headers
-	corsMux := middlewareCors(mux)
+	corsMux := middlewareCors(r)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -33,4 +41,26 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getHitsCountHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits)
+}
+
+func (cfg *apiConfig) resetHitsCountHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Hits count reset to 0")
 }
